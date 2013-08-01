be4844c7f983a1521963b3389f75494d7f66ce9c
Melissa Halfon <msmahliss@gmail.com>
2013-08-01 21:47:40 +0000

    web.js

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
-var fs = require('fs');
 var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 var text = fs.readFileSync("index.html","utf-8")
 
 app.get('/', function(request, response) {
  response.send(text);
- // response.send('Hello World With Such People In It!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080
+/*var port = process.env.PORT || 5000;*/
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
