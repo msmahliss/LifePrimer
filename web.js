12c9cae693f8ec55191b9f05db02636f32c72e78
msmahliss <msmahliss@gmail.com>
2013-11-18 17:39:21 +0000

    new web

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var port = process.env.PORT || 8080
-var path = require('path');
+//var path = require('path');
 var http    = require('http');
 var https   = require('https');
 //var snowbus = require('./SnowBus');
 
 app.set('port', process.env.PORT || 8080);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
 var text = fs.readFileSync("index.html","utf-8")
