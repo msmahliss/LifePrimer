ea884dfcff8fcbd640da04d6fa342395dec68fa0
msmahliss <msmahliss@gmail.com>
2013-11-18 18:29:32 +0000

    web.js

--- web.js
+++ web.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
-var port = process.env.PORT || 8080
 //var path = require('path');
 var http    = require('http');
 var https   = require('https');
 //var snowbus = require('./SnowBus');
 
+var app = express();
+
 app.set('port', process.env.PORT || 8080);
 //app.use(express.static(path.join(__dirname, 'public')));
-
+console.log(__dirname+ '/SnowBus');
 app.get('/', function(request, response) {
 var text = fs.readFileSync("index.html","utf-8")
  response.send(text);
 });
-/*
+
 http.createServer(app).listen(app.get('port'),function(){
   console.log("Listening on " + app.get('port'));
 });
-*/
+
