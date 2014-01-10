fce8363ababba9f8dd02b2bd598d487f6ff3bc60
Melissa Halfon <msmahliss@gmail.com>
2014-01-10 19:23:27 +0000

    Reinit LifePrimer

--- web.js
+++ web.js
@@ -1,15 +1,12 @@
-var express = require('express');
-var fs = require('fs');
-//var path = require('path');
-var http    = require('http');
-var https   = require('https');
-//var snowbus = require('./SnowBus');
+var express = require('express')
+ , fs      = require('fs')
+ , http    = require('http')
+ , path    = require('path');
 
 var app = express();
-
 app.set('port', process.env.PORT || 8080);
-//app.use(express.static(path.join(__dirname, 'public')));
-console.log(__dirname+ '/SnowBus');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(request, response) {
 var text = fs.readFileSync("index.html","utf-8")
  response.send(text);
@@ -18,4 +15,5 @@ var text = fs.readFileSync("index.html","utf-8")
 http.createServer(app).listen(app.get('port'),function(){
   console.log("Listening on " + app.get('port'));
 });
+
 
